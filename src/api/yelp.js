f9fd1334f3c50aec67059a5accd1a61a78754619
tudorpintea <tudorpintea98@gmail.com>
2020-07-24 20:24:29 +0300

    first screen finished

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'BearerH6hKFInhlNBEQdfNFjkTz-g09r5db2X_Qpx9M8Y9jFp6QMPYjx8ENe6QWjFaSs7hxD4iXdC6q7YOmceqXHExqe6X8iJprgWZrxhTnp2NaZYoTpdvDbLBnQ3kZeoVX3Yx'
+        Authorization: 'Bearer H6hKFInhlNBEQdfNFjkTz-g09r5db2X_Qpx9M8Y9jFp6QMPYjx8ENe6QWjFaSs7hxD4iXdC6q7YOmceqXHExqe6X8iJprgWZrxhTnp2NaZYoTpdvDbLBnQ3kZeoVX3Yx'
     }
 })
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,46 +1,41 @@
 import React, {useState, useEffect} from 'react'
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
 
-    const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-    const [errorMessage, setErrorMessage] = useState('')
+   const [term, setTerm] = useState('')
+   const [searchApi, results, errorMessage] = useResults();
+   const filterResultsByPrice = (price) => {
+      return results.filter(result => {
+          return result.price === price
+      })
+   }
 
-    const searchApi =  async (searchTerm) => {
-        
-        try { 
-
-        const response= await  yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: searchTerm,
-                location: 'san jose'
-            }
-        });
-        setResults(response.data.businesses)
-    } catch (err) {
-         setErrorMessage('Something wentt wrong')
-    };
-}
-    //call searchApi when component is first rendered
-
-
-   return <View>
+   return <>
        <SearchBar  
        term={term} 
        onTermChange={setTerm} 
        onTermSubmit={() => searchApi(term)}
        />
        {errorMessage ?  <Text>{errorMessage}</Text> : null}
-       <Text> We have founs {results.length} results</Text>
-   </View>
+       <ScrollView>
+       <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+       <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+       <ResultsList results={filterResultsByPrice('$$$')} title ='Big Spender'/>
+       </ScrollView>
+   </>
 };
 
 const styles = StyleSheet.create({
-
+  viewStyle: {
+      backgroundColor: 'white',
+      //marginLeft: 10
+  
+      flex: 1
+  }
 });
 
 export default SearchScreen;
