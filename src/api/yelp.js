1fb4cd28d6b52d8d5a044fde9c839f503d00a7f2
tudorpintea <tudorpintea98@gmail.com>
2020-07-24 10:33:45 +0300

    implementing search

---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'BearerH6hKFInhlNBEQdfNFjkTz-g09r5db2X_Qpx9M8Y9jFp6QMPYjx8ENe6QWjFaSs7hxD4iXdC6q7YOmceqXHExqe6X8iJprgWZrxhTnp2NaZYoTpdvDbLBnQ3kZeoVX3Yx'
+    }
+})
+
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp'
+
+const SearchScreen = () => {
+
+    const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const searchApi =  async (searchTerm) => {
+        
+        try { 
+
+        const response= await  yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses)
+    } catch (err) {
+         setErrorMessage('Something went wrong')
+    };
+}
+    //call searchApi when component is first rendered
+
+
+   return <View>
+       <SearchBar  
+       term={term} 
+       onTermChange={setTerm} 
+       onTermSubmit={() => searchApi(term)}
+       />
+       {errorMessage ?  <Text>{errorMessage}</Text> : null}
+       <Text> We have founs {results.length} results</Text>
+   </View>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
