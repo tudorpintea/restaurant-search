214876b173ae1cfca75a550adbb6d55b9592e611
tudorpintea <tudorpintea98@gmail.com>
2020-07-25 14:52:31 +0300

    app complete

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import {  createAppContainer } from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack'
 import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const navigator = createStackNavigator({
   Search: SearchScreen,
+  Results: ResultsShowScreen
   
 } , {
   initialRouteName: 'Search',
   defaultNavigationOptions: {
-    title: 'Business Search'
+    title: 'Business Search',
+    results: 'Details'
+
   }
 })
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,7 +5,6 @@ import useResults from '../hooks/useResults'
 import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
-
    const [term, setTerm] = useState('')
    const [searchApi, results, errorMessage] = useResults();
    const filterResultsByPrice = (price) => {
@@ -22,9 +21,18 @@ const SearchScreen = () => {
        />
        {errorMessage ?  <Text>{errorMessage}</Text> : null}
        <ScrollView>
-       <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
-       <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
-       <ResultsList results={filterResultsByPrice('$$$')} title ='Big Spender'/>
+       <ResultsList 
+       results={filterResultsByPrice('$')}
+        title='Cost Effective' 
+        />
+       <ResultsList 
+       results={filterResultsByPrice('$$')} 
+       title='Bit Pricier'
+       />
+       <ResultsList 
+       results={filterResultsByPrice('$$$')} 
+       title ='Big Spender'
+       />
        </ScrollView>
    </>
 };
